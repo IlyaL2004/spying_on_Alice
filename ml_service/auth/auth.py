b8594237c364e66a570df21aa549b75eb0a3c8bd
MDZhadnov <MDZhadnov@mai.education>
2024-12-23 19:20:11 +0300

    Auth done maybe?

--- ml_service/auth/auth.py
+++ ml_service/auth/auth.py
@@ -1,7 +1,7 @@
 from fastapi_users.authentication import CookieTransport, AuthenticationBackend, JWTStrategy
 from config import SECRET_JWT
 
-cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
+cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600, cookie_secure=False, cookie_httponly=True, cookie_path="/",)
 
 SECRET = SECRET_JWT
 
